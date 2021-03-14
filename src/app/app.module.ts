7dc424560b88fb33d6595552a17111f065fb237f
Thabo <ngubanethabo.ambrose@gmail.com>
2021-03-14 17:08:38 +0200

    Scaffold views and common components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './views/landing-page/landing-page.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { OurServicesComponent } from './views/our-services/our-services.component';
+import { ContactUsComponent } from './views/contact-us/contact-us.component';
+import { CareersComponent } from './views/careers/careers.component';
+import { CareerDetailComponent } from './views/career-detail/career-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    NavigationComponent,
+    FooterComponent,
+    OurServicesComponent,
+    ContactUsComponent,
+    CareersComponent,
+    CareerDetailComponent
   ],
   imports: [
     BrowserModule,
